eb6482969fe79e17b52f7651f3d40e835e0cd604
Ahmed Zedan <151939214+ahmedzedan02@users.noreply.github.com>
2025-04-28 19:14:11 +0300

    [1] [Time Beats: 100.00%] [Memory Beats: 36.21%] - LeetPush

--- Arrays Hashing/Two-Sum.cpp
+++ Arrays Hashing/Two-Sum.cpp
@@ -1,15 +1,17 @@
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
         for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
+            int complement = target - nums[i];
+            if (map.count(complement)) { 
+                return { map[complement], i };
             }
+            map[nums[i]] = i;
         }
         return {}; 
     }
