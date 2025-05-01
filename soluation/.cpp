b31d8ba0259012dd085d422336608c5a6eef73a0
Ahmed Zedan <151939214+ahmedzedan02@users.noreply.github.com>
2025-05-01 17:00:43 +0300

    solution for

--- soluation/.cpp
+++ soluation/.cpp
@@ -42,11 +42,4 @@ void solve() {
     }
     cout << "YES\n";
 }
-
-signed main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    int t; cin >> t;
-    while (t--) solve();
-    return 0;
 }
